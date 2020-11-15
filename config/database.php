0f22872ed083d37671a371dc63d3fd69c8f41363
Kevin Gonzalez <keving5726@gmail.com>
2020-11-15 19:10:57 -0400

    Add 'config' directory

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+return
+[
+    //'driver => 'mysql',
+    'driver' => 'pgsql',
+    'host' => '127.0.0.1',
+    'port' => 5432,
+    'database' => '',
+    'username' => '',
+    'password' => '',
+];
---
+++ config/global.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+define("NAME", "guitarshop");
+define("ENV", "production");
+define("DEBUG", "false");
+define("URL", "http://guitarshop");
+define("TIMEZONE", "UTC");
+define("LOCALE", "en");
+define("CONTROLLERS", __DIR__.'/../app/controller');
+define("VIEWS", __DIR__.'/../templates');
